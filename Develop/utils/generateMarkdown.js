6b1d84c1b83fee77f5c15ed91ff3fc6dab8f8fa1
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-05-07 18:15:52 -0700

    Update with correct links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,20 +2,19 @@
 // based on which license is passed in.
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
   switch(license) {
+    case 'AGPL 3.0':
+      return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+    case 'Apache 2.0':
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'GPL 3.0':
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'LGPL 3.0':
+      return '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
     case 'MIT': 
         return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    case 'LGPL 3.0':
-        return '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
     case 'MPL 2.0':
-        return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
-    case 'AGPL 3.0':
-        return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
-    case 'GPL 3.0':
-        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-    case 'Apache 2.0':
-        return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';  
     case 'Unlicense':
         return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
     default:
@@ -29,21 +28,21 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   switch(license) {
     case 'AGPL 3.0':
-        return 'Link: [![AGPL 3.0 License](https://opensource.org/licenses/AGPL-3.0';
+      return 'Link: [![AGPL 3.0 License](https://opensource.org/licenses/AGPL-3.0';
     case 'Apache 2.0':
-        return 'Link: [![Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)';
+      return 'Link: [![Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)';
     case 'GPL 3.0':
-        return 'Link: [![GPL 3.0 License](https://opensource.org/licenses/GPL-3.0)';
+      return 'Link: [![GPL 3.0 License](https://opensource.org/licenses/GPL-3.0)';
     case 'LGPL 3.0':
-        return 'Link: [![LGPL 3.0 License](https://opensource.org/licenses/unlicense)';
+      return 'Link: [![LGPL 3.0 License](https://opensource.org/licenses/unlicense)';
     case 'MIT': 
-        return 'Link: [![MIT License](https://opensource.org/licenses/MIT)';
+      return 'Link: [![MIT License](https://opensource.org/licenses/MIT)';
     case 'MPL 2.0':
-        return 'Link: [![MPL 2.0 License](https://opensource.org/licenses/MPL-2.0)';
+      return 'Link: [![MPL 2.0 License](https://opensource.org/licenses/MPL-2.0)';
     case 'Unlicense':
-        return 'Link: [![The Unlicense](https://opensource.org/licenses/unlicense)';
+      return 'Link: [![The Unlicense](https://opensource.org/licenses/unlicense)';
     default:
-        return '';
+      return '';
   //all licenses at Open Source Initative: https://opensource.org/
   };
 };
